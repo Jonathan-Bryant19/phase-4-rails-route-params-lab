58af4e6b8bed375baa785219e08344291e03e681
Jonathan Bryant <spectrum19tech@gmail.com>
2022-03-14 15:19:36 -0700

    commit tree 37227c55ed3ac6a5c2d6650b9145a5f8c1aa90e5

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,39 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    
+    if (params[:name])
+      name = params[:name].capitalize()
+      if (Student.where({first_name: name}))
+        student = Student.where({first_name: name})
+      elsif (Student.where({last_name: name}))
+        student = Student.where({last_name: name})
+      end
+    else 
+      student = Student.all
+    end
+    byebug
+    render json: student
+
+    # if (params[:first_name])
+    #   student = Student.find_by(first_name: params[:first_name])
+    # elsif (params[:last_name])
+    #   student = Student.find_by(last_name: params[:last_name])
+    # else
+    #   student = Student.all
+    # end
+    # render json: student
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def grades
+  end
+
+  def highest_grade
   end
 
 end
